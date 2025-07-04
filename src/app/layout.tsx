9eb77f49ca9f7418abfe09b55af526ff681a8e65
김영현 <>
2025-07-04 18:21:54 +0900

    feat: hotjar 연동 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -145,6 +145,22 @@ export default function RootLayout({
         <meta property="al:web:url" content="https://pawcus.dev" />
         <meta property="al:web:should_fallback" content="true" />
         
+        {/* Hotjar Tracking */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:XXXXXXX,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+        
         {/* Google Analytics 4 */}
         <script
           async
