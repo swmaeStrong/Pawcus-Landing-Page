43c1217737841679eef9ea2ffbed8cd8d47519fe
김영현 <>
2025-07-04 18:23:17 +0900

    feat: hotjar id 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -151,7 +151,7 @@ export default function RootLayout({
             __html: `
               (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:XXXXXXX,hjsv:6};
+                h._hjSettings={hjid:6454623,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
