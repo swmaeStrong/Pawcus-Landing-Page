ccf3b730ac2e3ecb185ec11db127927d9eff21ca
kyh0726 <younghyun75398@gmail.com>
2025-08-19 20:42:18 +0900

    feat: apple-app-site 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,15 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json'
+          }
+        ]
+      },
+      {
         source: '/:path*',
         headers: [
           {
